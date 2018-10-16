3da4c23d07e572e8b67d07694ffd793e7b621fcb
Revilo1882 <oliverrharris@gmail.com>
2018-10-16 13:19:44 +0100

    correct spacing

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,18 +7,18 @@ export class Card extends Component {
             <div className={card.selected ? 'card card-selected' : 'card'}>
                 <h2 className='card__section card__section--name'>{card.name}</h2>
                 <h3 className='card__section card__section--sub'>
-                    {card.apr}% APR</h3>
+                    {card.apr} % APR</h3>
                 <h3 className='card__section card__section--sub'>
-                    {card.balanceTransferOfferDuration} months 
-                    <span className='card__section--extra'> balance transfer offer duration</span>
-                </h3>
-                <h3 className='card__section card__section--sub'>{
-                    card.purchaseOfferDuration} months 
-                    <span className='card__section--extra'> 0% purchase offer duration</span>
+                    {card.balanceTransferOfferDuration} months{' '}
+                    <span className='card__section--extra'>balance transfer offer duration</span>
                 </h3>
                 <h3 className='card__section card__section--sub'>
-                    £{card.creditAvailable} 
-                    <span className='card__section--extra'> credit available</span>
+                {card.purchaseOfferDuration} months{' '}
+                    <span className='card__section--extra'>0% purchase offer duration</span>
+                </h3>
+                <h3 className='card__section card__section--sub'>
+                    £{card.creditAvailable}{' '} 
+                    <span className='card__section--extra'>credit available</span>
                 </h3>
             </div>
         );
